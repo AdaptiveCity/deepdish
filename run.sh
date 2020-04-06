67cdd84d8300f83750d24a96c16df25647a8e63a
Matthew Danish <mrd45@cam.ac.uk>
2020-04-06 14:40:39 +0100

    some renames

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 docker run --gpus 1 --net=host -it --rm \
        -e XAUTHORITY=/home/mrd45/.Xauthority \
        -e DISPLAY="$DISPLAY" \
-       -v /local/scratch-2/mrd45/src/deep_sort_yolov3:/tracker \
        -v $PWD:/work \
-       -v "$HOME":/home/mrd45:ro \
+       -v "$HOME":"$HOME":ro \
        -u `id -u`:`id -g` \
-       mrdanish/deep_sort_yolov3 $*
+       deepdish $*
