878ed40b996221a23ae471bafb615c5abd7618fe
Matthew Danish <mrd45@cam.ac.uk>
2022-10-24 17:28:53 +0100

    make sure mobilenet1/tflite-demo uses the tflite encoder

--- mobilenet1-demo.sh
+++ mobilenet1-demo.sh
@@ -4,6 +4,6 @@ INP="${1:-chall1.mp4}"
 OUT="${2:-out_$INP}"
 
 echo "Input: $INP, Output: $OUT"
-python3 deepdish.py  --model detectors/mobilenet/ssdmobilenetv1.tflite --labels detectors/mobilenet/labels.txt --encoder-model encoders/mars-64x32x3.pb --input "$INP" --output "$OUT" \
+python3 deepdish.py  --model detectors/mobilenet/ssdmobilenetv1.tflite --labels detectors/mobilenet/labels.txt --encoder-model encoders/mars-64x32x3.tflite --input "$INP" --output "$OUT" \
         --3d --sensor-width-mm 6.69 --sensor-height-mm 5.55 --focallength-mm 3.2 --elevation-m 1 --tilt-deg 80 --roll-deg 0 --topdownview-size-m "5,5"
 #         --line '0,240,640,240' \
